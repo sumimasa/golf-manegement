63928a8223afccfcb77e49c78bd01640ba64ce1d
sumimasa <smngmsk1234@icloud.com>
2021-11-11 11:35:07 +0900

    scoreページ遷移編集

--- app/javascript/splash.js
+++ app/javascript/splash.js
@@ -1,3 +1,18 @@
 $(function(){
-  $('#splash').delay(1).fadeOut('slow'); 
+  $('#splash').delay(500).fadeOut('slow'); 
 });
+
+$(window).on('load',function(){
+  //ロゴを1.2秒でフェードアウトする記述
+  
+  //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+  $("#splash-new").delay(500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+  
+  $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+  
+  });
+  //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+  
+  
+  
+  });
